f1a843dc0b0ed39e67f34d95e1f29b48fbc27624
SkylerOnRadio <abhigyadulal@gmail.com>
2026-08-25 21:05:49 +0530

    Completed Leetcode Question 2, O(n) time O(1) space

--- ll/add.cpp
+++ ll/add.cpp
@@ -2,118 +2,71 @@
 #include <iostream>
 #include <vector>
 
+int getNum(ListNode *head) {
+  int num = 0;
+  int i = 1;
+
+  while (head != nullptr) {
+    num += (head->val * i);
+    i *= 10;
+    head = head->next;
+  }
+
+  return num;
+}
+
 ListNode *add(ListNode *l1, ListNode *l2) {
   if (l1 == nullptr && l2 == nullptr)
     return nullptr;
 
-  if (l1 == nullptr || l2 == nullptr)
+  if ((l1 == nullptr) ^ (l2 == nullptr))
     return l1 == nullptr ? l2 : l1;
 
-  ListNode *prev = nullptr;
-  ListNode *curr = l1;
-  ListNode *next;
-
-  while (curr != nullptr) {
-    next = curr->next;
-
-    curr->next = prev;
-
-    prev = curr;
-    curr = next;
-  }
-  l1 = prev;
-
-  prev = nullptr;
-  curr = l2;
-  next = nullptr;
-
-  while (curr != nullptr) {
-    next = curr->next;
-
-    curr = prev;
-
-    prev = curr;
-    curr = next;
-  }
-  l2 = prev;
+  ListNode *dummy = new ListNode(0);
 
   ListNode *tmp1 = l1;
-  ListNode *tmp2 = l1;
-
-  ListNode *dummy = new ListNode(0);
+  ListNode *tmp2 = l2;
   ListNode *res = dummy;
 
   int carry{0};
+
   while (tmp1 != nullptr || tmp2 != nullptr) {
-    int sum = tmp1->val + tmp2->val + carry;
+    int num1{0};
+    int num2{0};
+
+    if (tmp1 != nullptr) {
+      num1 = tmp1->val;
+      tmp1 = tmp1->next;
+    }
+    if (tmp2 != nullptr) {
+      num2 = tmp2->val;
+      tmp2 = tmp2->next;
+    }
+
+    int sum = num1 + num2 + carry;
+
     carry = sum / 10;
-    sum = sum % 10;
 
-    res->next = new ListNode(sum);
+    res->next = new ListNode(sum % 10);
     res = res->next;
-    tmp1 = tmp1->next;
-    tmp2 = tmp2->next;
   }
 
-  if ((tmp1 == nullptr) ^ (tmp2 == nullptr)) {
-    ListNode *tmp;
-    if (tmp1 == nullptr) {
-      tmp = tmp2;
-    } else {
-      tmp = tmp1;
-    }
-
-    while (tmp != nullptr) {
-      int sum = tmp->val + carry;
-      carry = sum / 10;
-      sum = sum % 10;
-
-      res->next = new ListNode(sum);
-      res = res->next;
-      tmp = tmp->next;
-    }
-  }
-
-  if (carry != 0) {
+  if (carry != 0)
     res->next = new ListNode(carry);
-  }
 
-  prev = nullptr;
-  curr = dummy;
-  next = nullptr;
-
-  while (curr != nullptr) {
-    next = curr->next;
-
-    curr->next = prev;
-
-    prev = curr;
-    curr = next;
-  }
-
-  return prev;
+  return dummy->next;
 }
 
 int main(int argc, char *argv[]) {
-  std::vector<int> list1 = {2, 4, 3};
-  std::vector<int> list2 = {5, 6, 4};
+  std::vector<int> num1 = {4, 0, 2, 1};
+  std::vector<int> num2 = {2, 1, 5};
 
-  ListNode *head1 = createList(list1);
-  ListNode *head2 = createList(list2);
+  ListNode *a = createList(num1);
+  ListNode *b = createList(num2);
 
-  ListNode *res = add(head1, head2);
+  ListNode *res = add(a, b);
 
-  ListNode *tmp = res;
-  int num{0};
-  int i{1};
-
-  while (tmp != nullptr) {
-    num += tmp->val * i;
-    i *= 10;
-    tmp = tmp->next;
-  }
-
-  std::cout << num << "\n";
+  std::cout << getNum(a) << " + " << getNum(b) << " = " << getNum(res) << "\n";
 
   return 0;
 }
